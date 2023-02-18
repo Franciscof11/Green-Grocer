8744c5f3b5a20401c076ff32f6609b3398f1b9ae
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-02-17 23:46:48 -0300

    feat : Sing in and Sing up buttons

--- lib/View/Pages/Auth/sing_in.dart
+++ lib/View/Pages/Auth/sing_in.dart
@@ -45,14 +45,6 @@ class SingIn extends StatelessWidget {
                                   fontWeight: FontWeight.w800,
                                 ))
                           ])),
-                      Text(
-                        'Produtos',
-                        style: GoogleFonts.cairo(
-                          fontSize: 30,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
                     ],
                   ),
                 ),
@@ -60,50 +52,111 @@ class SingIn extends StatelessWidget {
             ),
           ),
           SingleChildScrollView(
-            child: Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(50),
-                  ),
-                  color: Colors.white,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(50),
                 ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 22),
-                        child: CustomTextField(
-                          label: 'Email',
-                          prefixIcon: Icon(Icons.email_outlined),
-                        )),
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 20),
-                        child: CustomTextField(
-                          label: 'Senha',
-                          isSecret: true,
-                          prefixIcon: Icon(Icons.lock_outline),
-                        )),
-                    GestureDetector(
-                      child: Container(
-                        width: double.infinity,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 22),
+                      child: CustomTextField(
+                        label: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: CustomTextField(
+                        label: 'Senha',
+                        isSecret: true,
+                        prefixIcon: Icon(Icons.lock_outline),
+                      )),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 55,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Center(
-                          child: Text('Entrar',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 22)),
+                        )),
+                        onPressed: () {},
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 168, 57, 49),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey[400],
+                          thickness: 1.3,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Text(
+                          'Ou',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey[400],
+                          thickness: 1.3,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 55,
+                      child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 1.8,
+                                color: Colors.green,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              )),
+                          onPressed: () {},
+                          child: Text('Criar conta',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ))),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
