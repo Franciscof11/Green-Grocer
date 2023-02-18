8e3c4c4144dd6a55a3a8119c889b68f1c0a1be24
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-02-17 22:52:32 -0300

    feat : Custom text field with hidePassword

--- lib/View/Widgets/custom_text_input.dart
+++ lib/View/Widgets/custom_text_input.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField(
-      {super.key,
-      required this.label,
-      this.prefixIcon,
-      this.obscureText = false});
+class CustomTextField extends StatefulWidget {
   final String label;
   final Icon? prefixIcon;
-  final bool obscureText;
+  final bool isSecret;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    this.prefixIcon,
+    this.isSecret = false,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool obscureText = false;
+
+  @override
+  void initState() {
+    super.initState();
+    obscureText = widget.isSecret;
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       obscureText: obscureText,
       style: TextStyle(fontSize: 16),
       decoration: InputDecoration(
-        labelText: label,
+        labelText: widget.label,
+        suffixIcon: widget.isSecret
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    obscureText = !obscureText;
+                  });
+                },
+                icon:
+                    Icon(obscureText ? Icons.visibility : Icons.visibility_off),
+              )
+            : null,
         floatingLabelStyle: TextStyle(fontSize: 20),
-        prefixIcon: prefixIcon,
+        prefixIcon: widget.prefixIcon,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(18),
         ),
