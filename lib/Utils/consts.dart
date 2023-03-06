3c10c63b53dc0d53ed55e320ccdd8b25b4156ef7
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-03-06 09:06:23 -0300

    feat : Added Masks on TextFormField

--- lib/Utils/consts.dart
+++ lib/Utils/consts.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class ColorsClass {
   Color backgroundGreen = Color.fromRGBO(140, 195, 74, 1);
   Color secondRedFont = Color.fromRGBO(157, 61, 25, 1);
+}
+
+class Masks {
+  final cpfMask = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+  final phoneMask = MaskTextInputFormatter(
+    mask: '(##) #####-####',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
 }
--- lib/View/Widgets/custom_text_input.dart
+++ lib/View/Widgets/custom_text_input.dart
@@ -1,16 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatefulWidget {
   final String label;
   final Icon? prefixIcon;
   final bool isSecret;
-
+  final List<TextInputFormatter>? mask;
   const CustomTextField({
-    super.key,
+    Key? key,
     required this.label,
     this.prefixIcon,
     this.isSecret = false,
-  });
+    this.mask,
+  }) : super(key: key);
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -28,6 +31,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      inputFormatters: widget.mask,
       obscureText: obscureText,
       style: TextStyle(fontSize: 16),
       decoration: InputDecoration(
