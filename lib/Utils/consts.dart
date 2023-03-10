4b2488220a939f4237486649ef17e94fc65d985e
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-03-10 10:27:26 -0300

    feat: :sparkles: App Bar

--- lib/Utils/consts.dart
+++ lib/Utils/consts.dart
@@ -3,7 +3,8 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class ColorsClass {
   Color backgroundGreen = Color.fromRGBO(140, 195, 74, 1);
-  Color secondRedFont = Color.fromRGBO(157, 61, 25, 1);
+  Color redFontTitleApp = Color.fromRGBO(157, 61, 25, 1);
+  Color backgroundGrey = Color.fromRGBO(243, 241, 242, 1);
 }
 
 class Masks {
--- lib/View/Pages/home_page.dart
+++ lib/View/Pages/home_page.dart
@@ -1,21 +1,83 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:green_grocer/Utils/consts.dart';
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorsClass().backgroundGreen,
-      body: SafeArea(
+      backgroundColor: ColorsClass().backgroundGrey,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15, top: 8),
+            child: IconButton(
+              onPressed: () {},
+              icon: badges.Badge(
+                badgeContent: Text(
+                  '2',
+                  style: TextStyle(color: Colors.white),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: ColorsClass().backgroundGreen,
+                  size: 28,
+                ),
+              ),
+            ),
+          ),
+        ],
+        title: Text.rich(
+          TextSpan(
+              text: 'Green',
+              style: GoogleFonts.cairo(
+                fontSize: 40,
+                color: ColorsClass().backgroundGreen,
+                fontWeight: FontWeight.bold,
+              ),
+              children: [
+                TextSpan(
+                    text: 'grocer',
+                    style: GoogleFonts.cairo(
+                      fontSize: 40,
+                      color: ColorsClass().redFontTitleApp,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ]),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: double.infinity,
-              height: 200,
-              color: Colors.amber,
-              child: Center(child: Text('OLA')),
-            )
+            //
+            //Pesquisa
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 18),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(26),
+                  borderSide: BorderSide.none,
+                ),
+                prefixIcon: Icon(Icons.search_outlined),
+                prefixIconColor: ColorsClass().redFontTitleApp,
+                hintText: "Pesquise Aqui...",
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+
+            //
+            //GridView Com Seleção
           ],
         ),
       ),
